93911bedf21bc9e310984561575575297720dc46
bego46 <berlad46@gmail.com>
2025-05-17 16:03:16 -0500

    Agregando encabezados a los documentos necesarios

--- web/urls.py
+++ web/urls.py
@@ -1,3 +1,24 @@
+"""
+------------------------------------------------------------------------------
+Autor: Berlad Gonzalez Valenzuela para Developments Berlad (<DB/>)
+Proyecto: Portafolio personal de desarrollo web.
+Archivo: urls.py
+Fecha de inserción: 2025-05-17
+
+Este archivo es parte del repositorio oficial de Developments Berlad y está
+protegido por derechos de autor. No está permitida su copia, modificación
+ni redistribución sin autorización expresa.
+
+This file is part of the official Developments Berlad repository.
+It is protected by copyright and may not be copied, modified, or distributed
+without express permission.
+
+ℹ️ Para más información, consulte el archivo README (disponible en varios idiomas).
+ℹ️ For more information, see the README file (available in multiple languages).
+------------------------------------------------------------------------------
+
+"""
+
 from django.urls import path
 from . import views
 
