d8ba7a30578e833d5e19d3786de05449d29e3dcb
bego46 <berlad46@gmail.com>
2025-06-05 12:33:52 -0500

    Modificando nombres y archivos 3

---
+++ web/static/js/01particles.js
@@ -0,0 +1,26 @@
+/*
+------------------------------------------------------------------------------
+Autor: Berlad Gonzalez Valenzuela para Developments Berlad (<DB/>)
+Proyecto: Portafolio personal de desarrollo web.
+Archivo: particles.js
+Fecha de inserción: 2025-05-17
+
+Este archivo es parte del repositorio oficial de Developments Berlad y está
+protegido por derechos de autor. No está permitida su copia, modificación
+ni redistribución sin autorización expresa.
+
+This file is part of the official Developments Berlad repository.
+It is protected by copyright and may not be copied, modified, or distributed
+without express permission.
+
+ℹ️ Para más información, consulte el archivo README (disponible en varios idiomas).
+ℹ️ For more information, see the README file (available in multiple languages).
+------------------------------------------------------------------------------
+
+*/
+
+document.addEventListener('DOMContentLoaded', function() {
+    particlesJS.load('particles-js', 'static/projects/particles.json', function() {
+        console.log("💻 Partículas inicializadas!");
+    });
+});
